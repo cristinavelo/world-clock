4ab8b75932849331775fe198399baaea252f08c5
cristinavelo <cristinavelo1@gmail.com>
2024-05-09 16:20:38 +0200

    Add js to the project

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  //London
+  let londonElement = document.querySelector("#london");
+  londonDateElement = londonElement.querySelector(".date");
+  londonTimeElement = londonElement.querySelector(".time");
+  londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM DD, YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "hh:mm:SS [<small>]A[</small>]"
+  );
+
+  //Seoul
+  let seoulElement = document.querySelector("#seoul");
+  seoulDateElement = seoulElement.querySelector(".date");
+  seoulTimeElement = seoulElement.querySelector(".time");
+  seoulTime = moment().tz("Asia/Seoul");
+  seoulDateElement.innerHTML = seoulTime.format("MMMM DD, YYYY");
+  seoulTimeElement.innerHTML = seoulTime.format(
+    "hh:mm:SS [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
